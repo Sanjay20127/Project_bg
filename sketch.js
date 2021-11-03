90598bf68f162a344bb7d4d54f6a4d1328720e14
Sanjay20127 <86659073+Sanjay20127@users.noreply.github.com>
2021-11-03 19:45:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,31 +1,21 @@
-var btn_red;
-var btn_green;
-
-var r = 0;
-var g = 0;
-var b = 0;
 
 function setup() {
   createCanvas(400, 400);  
 }
 
 function draw() {
-  background(r,g,b);
-  function red_bg(){
-    r = 255;
-    g = 0;
-    b = 0;
-  }
-  function green_bg(){
-    r = 0;
-    g = 255;
-    b = 0;
-  }
+  
   if(keyIsDown(RIGHT_ARROW)){
-    red_bg();
+    background("red");
   }
   if(keyIsDown(DOWN_ARROW)){
-    green_bg();
+    background("blue");
+  }
+  if(keyIsDown(UP_ARROW)){
+    background("green");
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    background("yellow");
   }
 }
 
